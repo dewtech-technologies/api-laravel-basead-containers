30aa359c8a7af32022b59aae843f62476a7dfa89
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2023-08-03 18:15:25 -0300

    Create layer Repository

---
+++ application/app/Repositories/UserRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository extends BaseRepository
+{
+    /**
+     * @var User
+     */
+    protected $users;
+
+    public function __construct(User $model)
+    {
+        $this->users = new User();
+    }
+
+    public function getAll()
+    {
+        return $this->users->all();
+    }
+
+    public function getById($id)
+    {
+        return $this->users->find($id);
+    }
+    public function create(array $attributes)
+    {
+        return $this->users->create($attributes);
+    }
+    public function update($id, array $attributes)
+    {
+        return $this->users->find($id)->update($attributes);
+    }
+    public function delete($id)
+    {
+        return $this->users->find($id)->delete();
+    }
+
+}
